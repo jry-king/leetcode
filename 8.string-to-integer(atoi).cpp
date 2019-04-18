aec812976dab53beff328192d7e67bad66af3f9f
jry-king <2472559327@qq.com>
2019-04-18 15:30:01 +0800

    complete annotation for 8

--- 8.string-to-integer(atoi).cpp
+++ 8.string-to-integer(atoi).cpp
@@ -1,6 +1,11 @@
 #include <string>
 using namespace std;
 
+/*
+ * O[n] time complexity and O[1] space complexity
+ * 8 ms, faster than 99.67%
+ * 8.5 MB, less than 99.8%
+ */
 #include <limits.h>
 
 class Solution {
@@ -9,7 +14,7 @@ public:
     {
         int ans = 0;
         bool sign = true;   // positive or negative
-        bool num = false;   // whether the conversion has started
+        bool num = false;   // whether the conversion has started (also used to check when the conversion ends)
         for (int i = 0; i < str.length(); i++)
         {
             // skip prefix spaces
@@ -18,19 +23,21 @@ public:
                 continue;
             }
             // identify negative number
-            // '-' is valid only before 
+            // '-' is valid only before digits
             if (str[i] == '-' && !num)
             {
                 sign = false;
                 num = true;
                 continue;
             }
+            // identify plus sign, valid only before digits
             else if (str[i] == '+' && !num)
             {
                 sign = true;
                 num = true;
                 continue;
             }
+            // process digits, detect overflow beforehand
             else if (isdigit(str[i]))
             {
                 num = true;
@@ -43,29 +50,31 @@ public:
                     return INT_MIN;
                 }
                 ans *= 10;
-                int num = str[i] - '0';
-                if (sign && ans > INT_MAX - num)
+                int number = str[i] - '0';
+                if (sign && ans > INT_MAX - number)
                 {
                     return INT_MAX;
                 }
-                else if (!sign && ans < INT_MIN + num)
+                else if (!sign && ans < INT_MIN + number)
                 {
                     return INT_MIN;
                 }
                 if (sign)
                 {
-                    ans += num;
+                    ans += number;
                 }
                 else
                 {
-                    ans -= num;
+                    ans -= number;
                 }
             }
+            // process ends when reach invalid character or reach nondigit with the num flag on
             else
             {
                 return ans;
             }
         }
+        // process ends when reach the end of the string
         return ans;
     }
 };
